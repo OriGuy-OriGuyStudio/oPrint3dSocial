696734b6c7bbd65771f4b3070817fec85f31ed4b
Ori Guy Studio <origuy@origuystudio.com>
2025-03-05 23:25:28 +0200

    Refactor code and update Firebase configuration

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -2,8 +2,6 @@
 import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -16,10 +14,12 @@ const firebaseConfig = {
 }
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig)
 
 // Initialize Firebase Authentication and get a reference to the service
-export const auth = getAuth(app)
+const auth = getAuth(app)
 
 // Initialize Cloud Firestore and get a reference to the service
-export const db = getFirestore(app)
+const db = getFirestore(app)
+
+export { app, auth, db }
